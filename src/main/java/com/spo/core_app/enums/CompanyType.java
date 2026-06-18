0f6e16ec9145d9312c6be28bcfbe5c4a7fe931ff
gokul.s <kgokula48@gmail.com>
2026-06-18 13:13:19 +0530

    Task-2/SubTask-1 onboarding flow company model class created and procurement company model class defined with Annotaions

---
+++ src/main/java/com/spo/core_app/enums/CompanyType.java
@@ -0,0 +1,9 @@
+package com.spo.core_app.enums;
+
+public enum CompanyType {
+    MANUFACTURING,
+    FMCG,
+    IT,
+    ELECTRONICS
+
+}
---
+++ src/main/java/com/spo/core_app/models/company.java
@@ -0,0 +1,52 @@
+package com.spo.core_app.models;
+
+
+import com.spo.core_app.enums.CompanyType;
+import com.spo.core_app.enums.Status;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "companies")
+public class company extends GlobalRecord{
+
+    private String companyId;
+    private String legalName;
+    private String displayName;
+    @Enumerated
+    private CompanyType companyType;
+    @Enumerated
+    private Status status;
+    private String taxId;
+    private String taxRegNumber;
+    private String govRegNumber;
+    private String primaryContactNumber;
+    private String contactName;
+    private String contactEmail;
+    private String addressLine1;
+    private String addressLine2;
+    private String addressLine3;
+    private String city;
+    private String country;
+
+
+
+
+
+
+
+
+
+
+
+
+}
